101df4e2ba584f36617537223fbf32beace65558
ismaelce122 <ismaelce@hotmail.com>
2025-06-11 19:15:04 -0300

    Agendamento de Shows

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('oi')
+from fastapi import FastAPI # Importando a Biblioteca fastapi
+from pydantic import BaseModel # Importando a Biblioteca pydantic
+import mysql.connector as my
+
+app = FastAPI()
+
+def conectar_banco():
+    conexao = my.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '',
+        database = 'eventos_db'
+    )
+    return conexao
+
+class User(BaseModel):
+    nome: str
+    idade: int
+    email: str
+    cidade: str
+
+@app.get('/')
+def rotaUsuarios():
+    return 'oi'
